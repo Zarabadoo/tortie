3940fd648d7f5f17722d20fb7ea579d1e45b3c4d
Al Steffen <al@zarabadoo.org>
2019-05-04 22:34:37 -0400

    refactor: move components into a subdirectory

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { configure } from "@storybook/vue";
 
-const req = require.context("../../packages", true, /.stories.js$/);
+const req = require.context("../../packages/components", true, /.stories.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
